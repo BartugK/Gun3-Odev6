c63eb34bdd6524f2c5ac38e03cdeb6cd2c425162
Bartuğ Kaan <bartugkaan.celebi@outlook.com>
2021-02-04 23:45:23 +0300

    Gun4-Odev3 eklendi

---
+++ MyDictionaryOdev/MyDictonary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryOdev
+{
+    class MyDictionary<K,V>
+    {
+        
+        K[] key;
+        K[] tempKey;
+
+        V[] value;
+        V[] tempValue;
+
+
+
+        public MyDictionary()
+        {
+            
+            key = new K[0];
+            value = new V[0];
+        }
+
+        public void Add(K keyItem, V valueItem)
+        {
+            
+            tempKey = key;
+            key = new K[key.Length+ 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+            }
+
+            key[key.Length - 1] = keyItem;
+
+
+
+            tempValue = value;
+            value = new V[value.Length + 1];
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                value[j] = tempValue[j];
+            }
+            value[value.Length - 1] = valueItem;
+        }
+
+
+
+        public K[] Key { get { return key; } }
+
+        public V[] Value { get { return value; } }
+
+
+
+    }
+}
